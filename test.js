5c6d7cda510c4a666f91c1277d6f69fd40d65dfc
roadblock37 <Roadblock37@gmail.com>
2022-08-31 17:52:51 -0400

    hamburger menu continued

--- test.js
+++ test.js
@@ -3,9 +3,8 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
-const navToggle = document.getElementById("nav-toggle");
+const navToggle = document.querySelector(".nav-toggle");
 const links = document.querySelector(".links");
 navToggle.addEventListener("click", function () {
-    console.log("hamburger clicked")
 links.classList.toggle("show-links");
 });
